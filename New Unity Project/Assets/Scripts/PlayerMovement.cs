77b145c9621cc4357f1bdee5c3ebec90d65fe1b6
Valentin Bordeianu <valentin.bordeianu@gmail.com>
2020-12-02 16:59:07 +0100

    Got a basic 2D scene setup with cheap ass movement

---
+++ New Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+
+    Vector2 delta;
+
+    void Update()
+    {
+        delta.x = -Input.GetAxisRaw("Horizontal");
+        delta.y = Input.GetAxisRaw("Vertical");
+    }
+
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + delta * moveSpeed * Time.fixedDeltaTime);
+    }
+}
---
+++ New Unity Project/Assets/Scripts/SpriteOrderer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteOrderer : MonoBehaviour
+{
+    SpriteRenderer sr;
+
+    [Range(0f, 1f)]
+    public float bottomOffset;
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        float off = bottomOffset * sr.bounds.size.y;
+        sr.sortingOrder = Mathf.RoundToInt((sr.bounds.min.y + off) * 100f) * -1;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (sr == null) sr = GetComponent<SpriteRenderer>();
+
+        float off = bottomOffset * sr.bounds.size.y;
+        Gizmos.DrawSphere(new Vector3(transform.position.x, sr.bounds.min.y + off, transform.position.z), 0.1f);
+    }
+}
